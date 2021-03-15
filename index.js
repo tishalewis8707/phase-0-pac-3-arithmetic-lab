ee7f374786e37d191f625965c888fcc3c29b25a9
tishalewis8707 <tishalewis8707@gmail.com>
2021-03-14 22:55:49 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,51 @@
+var a = 1000
+var b = 100
+
+function add() {
+    return a += 5;
+}
+function subtract() {
+    return a -= 5;
+}
+function multiply() {
+    return a *= 5;
+
+}
+function divide() {
+    return a /= 5;
+
+}
+function add(a, b) {
+    return a += b
+}
+function subtract(a, b) {
+    return a -= b
+}
+
+function multiply(a, b) {
+    return a *= b
+
+}
+function divide(a, b) {
+    return a /= b
+}
+function increment(a) {
+    return a+1 ;
+     
+}
+
+function decrement(a){
+    return a-1 ;
+}
+ 
+function  makeInt(n) {
+    parseInt(n)
+    return parseInt(n,10)
+  
+}
+    
+function preserveDecimal(n) {
+    
+    return parseFloat(n,2.222)
+
+}    
